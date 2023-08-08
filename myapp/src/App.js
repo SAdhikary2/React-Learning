37ea29942d3d908ceb678d0ca89c7291e4575262
Sukalyan <sadhikary0055@gmail.com>
2023-08-08 18:39:55 +0530

    commit

---
+++ myapp/src/App.js
@@ -0,0 +1,11 @@
+function App(){
+    // return <h3>This is the first component</h3>
+    return <div>
+       {/* return only one tag */}
+        <h3>This is the first component</h3>
+        <p>Return only one tag that'swhy all things in the div section</p>
+    </div>
+    
+}
+
+export default App;
